b2a4acec3cacbc6eb8df3932a432409ca19cfa33
Parvathy Menon <parvathy.menon1997@gmail.com>
2016-11-01 16:55:54 -0700

    Completed Issue #6

--- VioletDroidApplication/app/src/main/java/com/example/violetdroidapplication/ClassDiagramEditorActivity.java
+++ VioletDroidApplication/app/src/main/java/com/example/violetdroidapplication/ClassDiagramEditorActivity.java
@@ -4,11 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class ClassDiagramEditorActivity extends AppCompatActivity implements View.OnClickListener {
 
     ClassDiagEditorView editorView;
     Button plusBtn;
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+    Button button5;
+    Button button6;
+    Button button7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,20 @@ public class ClassDiagramEditorActivity extends AppCompatActivity implements Vie
         plusBtn = (Button) findViewById(R.id.class_diag_editor_plus);
         plusBtn.setOnClickListener(this);
 
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+        button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(this);
+        button5 = (Button) findViewById(R.id.button5);
+        button5.setOnClickListener(this);
+        button6 = (Button) findViewById(R.id.button6);
+        button6.setOnClickListener(this);
+        button7 = (Button) findViewById(R.id.button7);
+        button7.setOnClickListener(this);
     }
 
     @Override
@@ -31,6 +53,20 @@ public class ClassDiagramEditorActivity extends AppCompatActivity implements Vie
             case R.id.class_diag_editor_plus:
                 editorView.addItem();
                 break;
+            case R.id.button1:
+                Toast.makeText(this, "Not implemented yet!", Toast.LENGTH_LONG).show();
+            case R.id.button2:
+                Toast.makeText(this, "Not implemented yet!", Toast.LENGTH_LONG).show();
+            case R.id.button3:
+                Toast.makeText(this, "Not implemented yet!", Toast.LENGTH_LONG).show();
+            case R.id.button4:
+                Toast.makeText(this, "Not implemented yet!", Toast.LENGTH_LONG).show();
+            case R.id.button5:
+                Toast.makeText(this, "Not implemented yet!", Toast.LENGTH_LONG).show();
+            case R.id.button6:
+                Toast.makeText(this, "Not implemented yet!", Toast.LENGTH_LONG).show();
+            case R.id.button7:
+                Toast.makeText(this, "Not implemented yet!", Toast.LENGTH_LONG).show();
         }
     }
 }
