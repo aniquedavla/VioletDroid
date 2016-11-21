a3d4356e3a4a4f7b7b8d86ce43039bf92e15bf46
Vishaal Prasad <vishaalprasad@MacBook-Pro-2.local>
2016-11-20 16:24:34 -0800

    added comments to Paints.java

--- VioletDroidApplication/app/src/main/java/com/example/violetdroidapplication/Paints.java
+++ VioletDroidApplication/app/src/main/java/com/example/violetdroidapplication/Paints.java
@@ -17,10 +17,8 @@ public class Paints {
 
     private static int SELECTED_BG_PAINT = Color.parseColor("#DBE9F9");  // color of selected item
 
-    private static Context ctx;
-
     /**
-     * Call this in the MainActivity
+     * Call this in the MainActivity, sets all the Paint objects to be used later on
      */
     public static void initializePaints(Context ctx) {
         defaultTextTitlePaint = new Paint();
@@ -41,24 +39,43 @@ public class Paints {
         defaultBgPaint.setStyle(Paint.Style.FILL);
     }
 
+    /**
+     * @return the default Paint to be used with non-title text
+     */
     public static Paint getDefaultTextPaint() {
         return defaultTextPaint;
     }
 
+    /**
+     * @return the default Paint to be used with Title text
+     */
     public static Paint getDefaultTextTitlePaint() {
         return defaultTextTitlePaint;
     }
 
+    /**
+     * @return the default Paint to be used with the Outline of a Class Item
+     */
     public static Paint getDefaultOutlinePaint() {
         return defaultOutlinePaint;
     }
 
+    /**
+     * @param selected changes background color to blue if true, white otherwise
+     * @return the default Paint to be used with the background
+     */
     public static Paint getDefaultBgPaint(boolean selected) {
         defaultBgPaint.setColor(selected ? SELECTED_BG_PAINT : Color.WHITE);
 
         return defaultBgPaint;
     }
 
+    /**
+     * convert Px to Dp
+     * @param px a number in pixels unit
+     * @param ctx Context used to calculate the conversion
+     * @return px represented as dp for this device
+     */
     private static int dpFrompx(int px, Context ctx) {
 
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
